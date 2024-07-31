75bf2f2262029334389010d47a2bb663341b1328
KiARC <kiarc@kiarc.win>
2024-07-31 17:36:56 -0400

    Add flavor CLI arg to jan
    See the helptext for an explanation

--- src/bin/jan.rs
+++ src/bin/jan.rs
@@ -1,4 +1,4 @@
-use splaycompress::{compress8, decompress8};
+use splaycompress::{compress, decompress, Flavor};
 use std::io::{stdin, stdout};
 
 use clap::Parser;
@@ -9,15 +9,32 @@ struct Args {
     /// Whether to decompress instead of compress.
     #[arg(short, long)]
     decompress: bool,
+
+    /// Flavor of the algorithm to use. Defaults to 8bit which is many times faster but slightly worse at compressing.
+    #[clap(value_enum)]
+    #[arg(short, long, default_value = "bit8")]
+    flavor: CLIFlavor,
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum CLIFlavor {
+    Bit8,
+    Bit16BE,
+    Bit16LE,
 }
 
 fn main() {
     let r = stdin().lock();
     let w = stdout().lock();
     let args = Args::parse();
+    let flavor = match args.flavor {
+        CLIFlavor::Bit8 => Flavor::Symbol8,
+        CLIFlavor::Bit16BE => Flavor::Symbol16BE,
+        CLIFlavor::Bit16LE => Flavor::Symbol16LE,
+    };
     if args.decompress {
-        decompress8(r, w).unwrap();
+        compress(flavor, r, w).unwrap()
     } else {
-        compress8(r, w).unwrap();
+        decompress(flavor, r, w).unwrap()
     }
 }
